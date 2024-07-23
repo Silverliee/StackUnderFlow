db336eefb84ae595ff6a2390b4798614aedc9e16
ApOxX <nourdinearbaoui@gmail.com>
2024-07-23 18:53:48 +0200

    add layout for group

---
+++ StackUnderFlowMobile/app/src/main/java/com/example/stackunderflow/ui/group/GroupFragment.kt
@@ -0,0 +1,31 @@
+package com.example.stackunderflow.ui.group
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.stackunderflow.R
+
+
+class GroupFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_group, container, false)
+    }
+
+}
